ae659f620869921de00356d06a322a3f9746cf0e
Orest <galenzao@gmail.com>
2025-03-02 13:07:05 +0200

    Continue codding

--- C/cmatrix.c
+++ C/cmatrix.c
@@ -3,13 +3,19 @@
 #include<unistd.h>
 #include<time.h>
 
+struct strem {
+  int y; // For head
+  int x; // For row
+  char str[100]; // For stream of characters
+}
+
 int main() {
   char symbol1 = (rand() % (126 - 33 + 1)) + 33;
   char symbol2 = (rand() % (126 - 33 + 1)) + 33;
   char symbol3 = (rand() % (126 - 33 + 1)) + 33;
 
   while(1) {
-  //  printf("\e[1;1H\e[2J");
+  //  printf("\e[4;1H\e[2J");
    usleep(50000);
    printf("%c ", symbol1);
    printf("%c ", symbol2);
