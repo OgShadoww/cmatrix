58ac93b40d2bdc30f21e8edb2b3ac0a9abe776a6
Orest <galenzao@gmail.com>
2025-03-02 11:59:48 +0200

    Add readme files

--- C/cmatrix.c
+++ C/cmatrix.c
@@ -1,7 +1,23 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<time.h>
 
 int main() {
-  
+  char symbol1 = (rand() % (126 - 33 + 1)) + 33;
+  char symbol2 = (rand() % (126 - 33 + 1)) + 33;
+  char symbol3 = (rand() % (126 - 33 + 1)) + 33;
 
+  while(1) {
+  //  printf("\e[1;1H\e[2J");
+   usleep(50000);
+   printf("%c ", symbol1);
+   printf("%c ", symbol2);
+   printf("%c \n", symbol3);
+   symbol1 = (rand() % (126 - 33 + 1) + 33);
+   symbol2 = (rand() % (126 - 33 + 1) + 33);
+   symbol3 = (rand() % (126 - 33 + 1) + 33);
+  }
+  
   return 0;
 }
